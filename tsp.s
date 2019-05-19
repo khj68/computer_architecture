7caaf9a1b868aebc88cf10305b879a56ee6897d8
Jueun-Park <elpion19@gmail.com>
2019-05-19 16:31:15 +0900

    store index i in stack

--- tsp.s
+++ tsp.s
@@ -114,7 +114,7 @@ main:
 	nop
 	jal		print_path
 	nop
-	li   $v0, 10	# terminate program
+	li		$v0, 10		# terminate program
     syscall
 
 print_path:
@@ -143,11 +143,12 @@ print_path:
 		nop
 		jr		$ra				# jump to $ra
 		nop
+
 save_path:
-	li		$t1, 7
-	li		$t3, 0
+	# li		$t1, 7
+	li		$t3, 0	# i
 	L3:
-		bge		$t3, $t1, save_path_end
+		bge		$t3, 7, save_path_end
 		sll		$t4, $t3, 2
 		la 		$s3, current_path	# $s3 = cur
 		add		$s3, $s3, $t4		# current_path[i]
@@ -157,50 +158,55 @@ save_path:
 		add		$s3, $s3, $t4
 		sw		$s1, 0($s3)
 
-		addiu	$t3, $t3, 1
-		b		L3			# branch to L3
+		addiu	$t3, $t3, 1		# i++
+		b		L3				# branch to L3
 		nop
 	save_path_end:
 		jr		$ra
 # TODO: debugging
-dfs:  # $a0 - n,  $a1 - depth, $f14 - sum, $t2 - i
+dfs:  # $a0 - n,  $a1 - depth, $f14 - sum, $s4 - i
 	beq		$a1, 6, dfs_end   # if depth == 6 then end
 	nop
-	addi	$sp, $sp, -40
-	sw		$ra, 32($sp)
-	sw		$a0, 24($sp)
-	sw		$a1, 16($sp)
-	s.d		$f14, 8($sp)	# 8 byte double
-	sw		$s7, 0($sp)		# &visit[i] of caller
+	addi	$sp, $sp, -48
+	sw		$ra, 40($sp)
+	sw		$a0, 36($sp)
+	sw		$a1, 24($sp)
+	s.d		$f14, 16($sp)	# 8 byte double
+	sw		$s7, 8($sp)		# &visit[i] of caller
+	sw		$s4, 0($sp)		# i index
 
-	li		$t2, 0   # $t2 is i index
+	li		$s4, 0   # $s4 is i index
 	L2:  # for loop
-		addi	$t2, $t2, 1			# ++i
-		beq		$t2, $a0, L2		# if i == n goto L2 # TODO: jueun:C코드에서 어느 부분이지
+		addi	$s4, $s4, 1			# ++i
+		# beq		$s4, $a0, L2		# if i == n goto L2 (visit이 제대로 작동 안 할까봐...)
+		# nop
+		bgt		$s4, 6, dfs_end 	# if i > 6 then end recursive call
 		nop
-		bgt		$t2, 6, dfs_end 	# if i > 6 then end recursive call
-		nop
-		sll		$t3, $t2, 2			# index processing
+		
+		sll		$t3, $s4, 2			# index processing
 		la		$s1, visit			# load visit address
 		add		$s7, $t3, $s1		# $s7 = address of visit[i]
 		lw		$t5, 0($s7)			# $t5 = visit[i]
 		beq		$t5, 1, L2			# if visit[i] == 1 continue;
 		nop
+		
 		la		$t0, arr			# $t0 = &arr
 		mul		$t1, $a0, 7			# col processing; $t1 = n * 7
-		add		$t1, $t1, $t2		# row proceesing; $ti = n * 7 + i
+		add		$t1, $t1, $s4		# row proceesing; $ti = n * 7 + i
 		mul		$t1, $t1, 8			# address processing; size of double
 		add		$t0, $t0, $t1		# $t0 = &arr[n][i]
 		l.d		$f4, 0($t0)			# $f4 = arr[n][i]
 		add.d	$f0, $f4, $f14		# $f0 = sum + arr[n][i]
 		ldc1	$f2, ans			# $f2 = ans
-		c.lt.d	$f2, $f0			# sum + arr[n][i] > ans then L2 (inverse condition)
+		
+		c.lt.d	$f2, $f0			# if sum + arr[n][i] > ans then L2 (inverse condition)
 		bc1t	L2
 		nop
+		
 		addi	$t5, $zero, 1		# $t5 = 1
 		sw		$t5, 0($s7) 		# visit[i] = 1
 		la		$s1, cities			# $s1 = &cities[0]
-		mul 	$s3, $t2, 12		# i * 12 (size of struct)
+		mul 	$s3, $s4, 12		# i * 12 (size of struct)
 		add 	$s1, $s1, $s3		# $s1 = &cities[i].num
 		lw		$t6, 0($s1)			# $t6 = cities[i].num
 		addi	$t7, $a1, 1			# depth+1
@@ -210,21 +216,16 @@ dfs:  # $a0 - n,  $a1 - depth, $f14 - sum, $t2 - i
 		sw		$t6, 0($s2)			# current_path[i] = cities[i].num
 
 		# save next argument
-		move	$a0, $t2			# n = i 
+		move	$a0, $s4			# n = i 
 		move	$a1, $t7			# depth = depth+1 
 		mfc1    $zero, $f6			# $f6 = 0.0
 		sub.d	$f14, $f0, $f6		# move $f0(sum+arr[n][i]) to $f14
 		jal		dfs		# recursive call
 		nop
-		sw		$zero, 0($s7)   	# visit[i] = 0
 		
-		lw		$s7, 0($sp)
-		l.d		$f14, 8($sp)
-		lw		$a1, 16($sp)
-		lw		$a0, 24($sp)
-		lw		$ra, 32($sp)
-		addi	$sp, $sp, 40
-		jr		$ra
+		sw		$zero, 0($s7)   	# visit[i] = 0
+		j		L2					# jump to L2
+		
 	dfs_end:
 		la		$t0, arr
 		mul		$t1, $a0, 7
@@ -235,23 +236,25 @@ dfs:  # $a0 - n,  $a1 - depth, $f14 - sum, $t2 - i
 		la		$t9, ans			# $t9 = &ans
 		l.d		$f6, 0($t9)			# $f6 = ans
 		c.lt.d	$f14, $f6			# if sum < ans
-		bc1t	save				# save_path
+		bc1t	save				# then goto save_path
 		nop
-		lw		$s7, 0($sp)
-		l.d		$f14, 8($sp)
-		lw		$a1, 16($sp)
-		lw		$a0, 24($sp)
-		lw		$ra, 32($sp)
-		addi	$sp, $sp, 40
+		lw		$s4, 0($sp)
+		lw		$s7, 8($sp)
+		l.d		$f14, 16($sp)
+		lw		$a1, 24($sp)
+		lw		$a0, 32($sp)
+		lw		$ra, 40($sp)
+		addi	$sp, $sp, 48
 		jr		$ra
 	save: 
 		s.d		$f14, 0($t9)		# ans = sum
 		jal		save_path
 		nop
-		lw		$s7, 0($sp)
-		l.d		$f14, 8($sp)
-		lw		$a1, 16($sp)
-		lw		$a0, 24($sp)
-		lw		$ra, 32($sp)
-		addi	$sp, $sp, 40
-		jr $ra
+		lw		$s4, 0($sp)
+		lw		$s7, 8($sp)
+		l.d		$f14, 16($sp)
+		lw		$a1, 24($sp)
+		lw		$a0, 32($sp)
+		lw		$ra, 40($sp)
+		addi	$sp, $sp, 48
+		jr		$ra
